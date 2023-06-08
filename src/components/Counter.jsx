3f809a166e05595723b4b4ab067078e7b6408cf3
chihebchabou <chihebchaboutech@gmail.com>
2023-06-08 11:56:38 +0100

    redux starter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Counter = () => {
+  return (
+    <>
+      <div className="mb-3">
+        <span>0</span>
+      </div>
+      <div className="flex justify-content-around">
+        <button className="width-100">+</button>
+        <button className="width-100">-</button>
+        <button className="width-100">+10</button>
+        <button className="width-100">+ Async</button>
+      </div>
+    </>
+  );
+};
